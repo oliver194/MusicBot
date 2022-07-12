ab76bef2873b38925b1db7280c6d81c1b7f850be
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-12 11:17:39 +0100

    Update OtherUtil.java

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -184,7 +184,7 @@ public class OtherUtil
         try
         {
             Response response = new OkHttpClient.Builder().build()
-                    .newCall(new Request.Builder().get().url("https://api.github.com/repos/jagrosh/MusicBot/releases/latest").build())
+                    .newCall(new Request.Builder().get().url("https://raw.githubusercontent.com/oliver194/MusicBot/master/version.json").build())
                     .execute();
             ResponseBody body = response.body();
             if(body != null)

2e647e8c3fa6a9f053a453e5c6a4e4df1194b02c
oliver194 <100048880+oliver194@users.noreply.github.com>
2023-01-30 21:30:23 +0000

    Update JMusicBot.java

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -88,7 +88,7 @@ public class JMusicBot
         
         AboutCommand aboutCommand = new AboutCommand(Color.decode("#584DFF"),
                                 "a modified version of [JMusicBot.](https://github.com/jagrosh/MusicBot) (v" + OtherUtil.getCurrentVersion() + ")",
-                                new String[]{"Modified version of JMusicBot", "High-quality music playback", "FairQueue™ Technology", "Easy to host yourself", "Modified for Huaweicord."},
+                                new String[]{"Modified version of JMusicBot", "High-quality music playback", "FairQueue™ Technology", "Easy to host yourself"},
                                 RECOMMENDED_PERMS);
         aboutCommand.setIsAuthor(false);
         aboutCommand.setReplacementCharacter("\uD83C\uDFB6"); // 🎶
